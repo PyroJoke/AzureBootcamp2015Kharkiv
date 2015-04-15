aee1a2a2a9625be54691471b1872a5c1ce94bd3c
Dmytro Mykhailov <dmytro_mykhailov@epam.com>
2015-04-15 14:31:19 +0300

    Added basic version of web site management via resource group

---
+++ CustomerManagerStandard/Automation/ResourceGroup.ps1
@@ -0,0 +1,79 @@
+﻿Param(
+    [string][Parameter(Mandatory=$true)] $WebSiteName,
+    [string][Parameter(Mandatory=$true)] $ProjectFile,
+    [string] $ResourceGroupName = $WebSiteName,
+    [string] $StorageAccountName = $ResourceGroupName.ToLowerInvariant() + "storage",
+    [string] $ResourceGroupLocation = "West Europe",
+    [string] $StorageContainerName = $WebSiteName.ToLowerInvariant(),
+    [string] $TemplateFile = '.\Templates\WebSiteDeploySQL.json',
+    [string] $TemplateParametersFile = '.\Templates\WebSiteDeploySQL.param.dev.json',
+    [string] $LocalStorageDropPath = '.\StorageDrop',
+    [string] $AzCopyPath = '.\Tools\AzCopy.exe'
+)
+
+$VerbosePreference = "Continue";
+$ErrorActionPreference = "Stop";
+
+$AzCopyPath = [System.IO.Path]::Combine($PSScriptRoot, $AzCopyPath);
+$TemplateFile = [System.IO.Path]::Combine($PSScriptRoot, $TemplateFile);
+$TemplateParametersFile = [System.IO.Path]::Combine($PSScriptRoot, $TemplateParametersFile);
+$LocalStorageDropPath = [System.IO.Path]::Combine($PSScriptRoot, $LocalStorageDropPath);
+
+#Build the application
+$publishXmlFile = ".\WebDeployPackage.pubxml";
+
+& "$env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $ProjectFile `
+    /p:VisualStudioVersion=12.0 `
+    /p:DeployOnBuild=true `
+    /p:DesktopBuildPackageLocation=$LocalStorageDropPath `
+    /p:PublishProfile=WebDeployPackage.pubxml;
+
+Switch-AzureMode -Name AzureServiceManagement;
+
+#Create new Azure Storage if it does not exist
+if(!(Test-AzureName -Storage $StorageAccountName)) {
+    $storageAcct = New-AzureStorageAccount -StorageAccountName $StorageAccountName -Location $ResourceGroupLocation -Verbose;
+    if ($storageAcct)
+    {
+        Write-Verbose "[Finish] creating $Name storage account in $Location location"
+    }
+    else
+    {
+        throw "Failed to create a Windows Azure storage account. Failure in New-AzureStorage.ps1"
+    }
+}
+
+#Copy application package to the storage
+$storageAccountKey = (Get-AzureStorageKey -StorageAccountName $StorageAccountName).Primary
+$storageAccountContext = New-AzureStorageContext $StorageAccountName (Get-AzureStorageKey $StorageAccountName).Primary
+$dropLocation = $storageAccountContext.BlobEndPoint + $StorageContainerName
+& "$AzCopyPath" """$LocalStorageDropPath"" $dropLocation /DestKey:$storageAccountKey /S /Y"
+
+$dropLocationSasToken = New-AzureStorageContainerSASToken -Container $StorageContainerName -Context $storageAccountContext -Permission r 
+$dropLocationSasToken = ConvertTo-SecureString $dropLocationSasToken -AsPlainText -Force
+
+#Define sql server
+$sqlServerName = "dmresource1server"
+$sqlDbName = "dmres1"
+$sqlServerAdminLogin = "userDB"
+$plainTextPassword = "P{0}!" -f ([System.Guid]::NewGuid()).Guid.Replace("-", "").Substring(0, 10);
+$sqlServerAdminPassword = ConvertTo-SecureString $plainTextPassword -AsPlainText -Force
+
+Switch-AzureMode AzureResourceManager;
+New-AzureResourceGroup -Name $ResourceGroupName `
+                       -Location $ResourceGroupLocation `
+                       -TemplateFile $TemplateFile `
+                       -TemplateParameterFile $TemplateParametersFile `
+                       -dropLocation $dropLocation `
+                       -dropLocationSasToken $dropLocationSasToken `
+                       -sqlServerName $sqlServerName `
+                       -sqlServerLocation $ResourceGroupLocation `
+                       -sqlServerAdminLogin $sqlServerAdminLogin `
+                       -sqlServerAdminPassword $sqlServerAdminPassword `
+                       -sqlDbName $sqlDbName `
+                       -webSiteName $webSiteName `
+                       -webSiteLocation $ResourceGroupLocation `
+                       -webSiteHostingPlanName "Standard2instances" `
+                       -webSiteHostingPlanSKU "Free" `
+                       -webSitePackage "CustomerManager.zip" `
+                       -Force -Verbose
