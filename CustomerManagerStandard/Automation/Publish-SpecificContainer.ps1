6d57f4a285c13c448288549a1c22930682b26399
Dmytro Mykhailov <dmytro_mykhailov@epam.com>
2015-04-16 16:52:17 +0300

    Organized deployment scripts a bit. Added deployment by specified container.

--- CustomerManagerStandard/Automation/Publish-SpecificContainer.ps1
+++ CustomerManagerStandard/Automation/Publish-SpecificContainer.ps1
@@ -1,11 +1,8 @@
 Param(
     [string][Parameter(Mandatory=$true)] $WebSiteName,
-	[string][ValidateScript({Test-Path $_ -PathType 'Leaf'})] [Parameter(Mandatory=$true)] $ProjectFile,
+	[string][Parameter(Mandatory=$true)] $StorageContainerName,
 	[string] $ResourceGroupName = $WebSiteName,
 	[string] $StorageAccountName = $ResourceGroupName.ToLowerInvariant() + "storage",
-    [string] $StorageContainerName = ("{0}-{1}" -f $WebSiteName.ToLowerInvariant(), (Get-Date -format "hh-mm-ss-dd-mm-yyyy")),
-	[string] $LocalStorageDropPath = '.\StorageDrop',
-    [string] $AzCopyPath = '.\Tools\AzCopy.exe',
 	[string] $webSitePackage = "CustomerManager.zip",
     [string] $TemplateFile = '.\Templates\PublishWebApp.json'
 )
@@ -13,31 +10,16 @@ Param(
 $ErrorActionPreference = "Stop";
 $wasServiceManagementMode = Get-Module -Name Azure -ListAvailable;
 
-$AzCopyPath = [System.IO.Path]::Combine($PSScriptRoot, $AzCopyPath);
 $TemplateFile = [System.IO.Path]::Combine($PSScriptRoot, $TemplateFile);
-$LocalStorageDropPath = [System.IO.Path]::Combine($PSScriptRoot, $LocalStorageDropPath);
-
-$publishXmlFile = ".\WebDeployPackage.pubxml";
-& "$env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $ProjectFile `
-    /p:VisualStudioVersion=12.0 `
-    /p:DeployOnBuild=true `
-    /p:DesktopBuildPackageLocation=$LocalStorageDropPath `
-    /p:PublishProfile=WebDeployPackage.pubxml;
-
 	
 Switch-AzureMode -Name AzureServiceManagement;
-#Copy application package to the storage
 $storageAccountKey = (Get-AzureStorageKey -StorageAccountName $StorageAccountName).Primary;
 $storageAccountContext = New-AzureStorageContext $StorageAccountName $storageAccountKey;
 $dropLocation = $storageAccountContext.BlobEndPoint + $StorageContainerName;
-& "$AzCopyPath" """$LocalStorageDropPath"" $dropLocation /DestKey:$storageAccountKey /S /Y";
-
-#Set drop location for msdeploy
 $dropLocationSasToken = New-AzureStorageContainerSASToken -Container $StorageContainerName -Context $storageAccountContext -Permission r;
 $dropLocationSasToken = ConvertTo-SecureString $dropLocationSasToken -AsPlainText -Force;
 
 Switch-AzureMode AzureResourceManager;
-
 $ResourceGroupLocation = (Get-AzureResourceGroup -Name $ResourceGroupName).Location;
 New-AzureResourceGroup -Name $ResourceGroupName `
                        -Location $ResourceGroupLocation `
