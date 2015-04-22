947d3f31c5d9dcabc47166cf33db88c71b817af9
Sergii Kryshtop <sergii_kryshtop@epam.com>
2015-04-23 01:46:43 +0300

    Keep account name in the ignored txt file, select the subscription after login

--- CustomerManagerStandard/Automation/Account.ps1
+++ CustomerManagerStandard/Automation/Account.ps1
@@ -1,14 +1,44 @@
 <#
+ # Create account.txt file with the username
+ Read-Host -Prompt "Please type the user account name in your subscription to use" | Out-File .\account.txt
+
  # Create PWD File with this command: 
- # Read-Host -AsSecureString | ConvertFrom-SecureString | out-file C:\Users\dmitry\pwdfile.txt
- #>
+ Read-Host -Prompt "Password" -AsSecureString | ConvertFrom-SecureString | out-file ($env:USERPROFILE + "\pwdfile.txt")
 
-$pwdFilePath = "C:\Users\dmitry\pwdfile.txt";
-if(!(Test-Path $pwdFilePath)) { throw "PWD File not found"; }
+#>
+Param(
+    [string]$PasswordFilePath = $env:USERPROFILE + "\pwdfile.txt",
 
-$username = "deployment@dmytromykhailovepam.onmicrosoft.com";
-$password =  cat $pwdFilePath | ConvertTo-SecureString;
+    [string]$SubscriptionName = "Free Trial"
+)
+
+if(!(Test-Path $PasswordFilePath) ) 
+{ 
+    Write-Error @"
+    PWD File not found. Please run the following command and type your Azure account password:
+    Read-Host -Prompt "Password" -AsSecureString | ConvertFrom-SecureString | out-file $env:USERPROFILE + "\pwdfile.txt"
+"@
+
+    throw "PWD File not found"; 
+}
+
+if(!(Test-Path  ".\account.txt") )
+{
+   throw "File account.txt not found"
+}
+
+
+$username = Get-Content .\account.txt
+$password =  cat $PasswordFilePath | ConvertTo-SecureString;
+
+Write-Host "Connecting as $username..."
 
 $deploymentCreds = New-Object System.Management.Automation.PSCredential($username, $password);
 
-Add-AzureAccount -Credential $deploymentCreds;
+Add-AzureAccount -Credential $deploymentCreds
+
+
+Write-Output "Selecting subscription $subscriptionName ..."
+Select-AzureSubscription -SubscriptionName $subscriptionName
+Set-AzureSubscription -SubscriptionName $subscriptionName
+$subscription = Get-AzureSubscription
